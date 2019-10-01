cddcecd7261adf1828e030f0dd5c7b1a6247b75a
Dylan Nirvana <dylannirvana@Dylans-MBP-2.cl.local>
2019-10-01 17:05:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import modal from './modal.mov';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <video controls autoplay src={modal}></video>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A spec sheet opens in a modal window on the Endless Aisle.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
